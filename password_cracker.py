97ca56f91458217c61bb9c84d9f53e25eeadef60
mateus-sartorio <mateussartorio39@gmail.com>
2025-01-01 03:31:56 +0000

    Feat: all tests passing

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,38 @@
 import hashlib
 
 def crack_sha1_hash(hash, use_salts = False):
-    return True
+    salts = []
+    if use_salts:
+        with open("known-salts.txt") as file:
+            for line in file:
+                line = line.strip()
+                salts.append(line)
+
+    with open("top-10000-passwords.txt") as file:
+        for line in file:
+            line = line.strip()
+
+            if use_salts:
+                for salt in salts:
+                    sha1 = hashlib.sha1()
+                    sha1.update(f"{salt}{line}".encode('utf-8'))
+                    hashed_password = sha1.hexdigest()
+
+                    if(hashed_password == hash):
+                        return line
+
+                    sha1 = hashlib.sha1()
+                    sha1.update(f"{line}{salt}".encode('utf-8'))
+                    hashed_password = sha1.hexdigest()
+
+                    if(hashed_password == hash):
+                        return line
+            else:
+                sha1 = hashlib.sha1()
+                sha1.update(line.encode('utf-8'))
+                hashed_password = sha1.hexdigest()
+
+            if(hashed_password == hash):
+                return line
+
+    return "PASSWORD NOT IN DATABASE"
